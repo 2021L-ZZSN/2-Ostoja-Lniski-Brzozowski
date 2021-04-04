ab682b68c72aad85bdc3d4000b3cc9fd97eddce1
robertostoja-lniski <robert.lniski@gmail.com>
2021-04-04 23:21:49 +0200

    json merger

--- utils/files_io.py
+++ utils/files_io.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def load_json(path):
@@ -16,3 +17,32 @@ def append_json(out_path, data):
     assert type(json_data) is dict and type(data) is dict, "Can't append json data, if the data is not a dict."
     json_data.update(data)
     write_json(out_path, json_data)
+
+
+def merge_jsons_from_dirs(out_path, *in_paths):
+    if os.path.exists(out_path):
+        os.remove(out_path)
+
+    for in_path in in_paths:
+        merge_jsons_from_dir(out_path, in_path)
+
+
+def _init_merged_content(out_path):
+    if os.path.exists(out_path):
+        init_content = load_json(out_path)
+    else:
+        init_content = []
+
+    return init_content
+
+
+def merge_jsons_from_dir(out_path, in_path):
+
+    merged_jsons = _init_merged_content(out_path)
+    for filename in os.listdir(in_path):
+        if filename.endswith(".json"):
+            json_data = load_json(in_path + '/' + filename)
+            assert type(json_data) is list
+            merged_jsons = merged_jsons + json_data
+
+    write_json(out_path, merged_jsons)
