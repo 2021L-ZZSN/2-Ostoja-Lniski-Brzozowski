a8bc4744e849cd3d7a6ddc7399d43509948f31d4
Dawid Brzozowski <dawidbrzozowski1998@gmail.com>
2021-06-07 18:56:26 +0200

    working version for colab

--- src/models/bert.py
+++ src/models/bert.py
@@ -1,6 +1,5 @@
 from transformers import AutoTokenizer, Trainer, BertForSequenceClassification, BertConfig, TrainingArguments
 
-from src.common.consts import RANDOM_STATE
 from src.common.data_preparation import KlejType, read_klej
 import torch
 from sklearn.model_selection import train_test_split
@@ -9,6 +8,8 @@ import numpy as np
 from src.models import MODEL_USED
 
 VALIDATION_SIZE = 0.1
+RANDOM_STATE = 42
+
 tokenizer = AutoTokenizer.from_pretrained(MODEL_USED)
 klej_in = read_klej(KlejType.IN, ("positive", "negative", "neutral"))
 train_data, test_data = klej_in["train"], klej_in["dev"]
@@ -31,8 +32,8 @@ train_data, val_data = train_test_split(
     random_state=RANDOM_STATE,
     stratify=[d["label"] for d in train_data])
 
-train_texts = [d["text"] for d in train_data][:5]
-train_labels = [d["label"] for d in train_data][:5]
+train_texts = [d["text"] for d in train_data]
+train_labels = [d["label"] for d in train_data]
 
 val_texts = [d["text"] for d in train_data]
 val_labels = [d["label"] for d in train_data]
@@ -72,8 +73,8 @@ test_dataset = KlejDataset(test_encodings, test_labels)
 training_args = TrainingArguments(
     output_dir='./results',  # output directory
     num_train_epochs=3,  # total number of training epochs
-    per_device_train_batch_size=16,  # batch size per device during training
-    per_device_eval_batch_size=64,  # batch size for evaluation
+    per_device_train_batch_size=8,  # batch size per device during training
+    per_device_eval_batch_size=16,  # batch size for evaluation
     warmup_steps=500,  # number of warmup steps for learning rate scheduler
     weight_decay=0.01,  # strength of weight decay
     logging_dir='./logs',  # directory for storing logs
